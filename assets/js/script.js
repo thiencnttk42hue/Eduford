2507fc7e1a180cb249dba8fe06e549ada6a3eeeb
unknown <ngocthien250800@gmail.com>
2022-06-09 16:42:32 +0700

    fisrt commit

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+let navBar = document.getElementById("js-header-navbar-list");
+
+let hideMenu = function() {
+    navBar.style.right = '-200px';
+}
+let showMenu = function() {
+    navBar.style.right = '0';
+}
